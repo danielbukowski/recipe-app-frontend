2ffba2c9a743d909bd25421d59cb957e30076905
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-01-04 17:16:25 +0100

    Remove not needed vue files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="flex flex-col items-center">
-    <img alt="Vue logo" class="logo mt-2" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper ">
-      <HelloWorld msg="You did it!" />
-
-      <nav class="justify-self-center">
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <div>
     <RouterView />
   </div>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <main>
-    <h1 class="text-center">Home view</h1>
-  </main>
-</template>
